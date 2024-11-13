1ec46454bef20097bffe1ec363544dd948487da4
Yahya Bagia <yahya.bagia@hotmail.com>
2024-11-13 09:09:34 +0530

    Expo Router Package Setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const index = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
